3bcb9fbb331c6af9ccddcffc91c0e28fb9bebd65
doanlv4 <coltech0089@gmail.com>
2017-03-28 21:46:02 +0700

    Doanlv4

---
+++ src/main/java/com/wms/persistents/model/AppParams.java
@@ -0,0 +1,101 @@
+package com.wms.persistents.model;
+
+/**
+ * Created by doanlv4 on 25/03/2017.
+ */
+import com.wms.base.BaseDTO;
+import com.wms.base.BaseModel;
+import com.wms.dto.AppParamsDTO;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+@Entity
+@DynamicUpdate
+@Table(name = "APP_PARAMS")
+public class AppParams extends BaseModel  {
+    private Long id;
+    private String code;
+    private String name;
+    private String status;
+    private String type;
+    private String parOrder;
+
+    public AppParams(Long id,String code, String name, String status, String type, String parOrder) {
+        this.id = id;
+        this.code = code;
+        this.name = name;
+        this.status = status;
+        this.type = type;
+        this.parOrder = parOrder;
+    }
+
+    public AppParams() {
+    }
+
+    @Id
+    @Column(name = "ID", unique = true, nullable = false)
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="SEQ_APP_PARAMS")
+    @SequenceGenerator(
+            name="SEQ_APP_PARAMS",
+            sequenceName="SEQ_APP_PARAMS",
+            allocationSize = 1,
+            initialValue= 1000
+    )
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "CODE")
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "STATUS")
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Column(name = "TYPE")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Column(name = "PAR_ORDER")
+    public String getParOrder() {
+        return parOrder;
+    }
+
+    public void setParOrder(String parOrder) {
+        this.parOrder = parOrder;
+    }
+
+    @Override
+    public BaseDTO toDTO() {
+        return new AppParamsDTO(id==null?"":id+"",code,name,status,type, parOrder);
+    }
+}
---
+++ src/main/java/com/wms/services/AppParamsServices.java
@@ -0,0 +1,32 @@
+package com.wms.services;
+
+/**
+ * Created by doanlv4 on 25/03/2017.
+ */
+import com.wms.base.BaseBusinessInterface;
+import com.wms.base.BaseServices;
+import com.wms.dto.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by duyot on 12/7/2016.
+ */
+@RestController
+@RequestMapping("/services/appParamsServices")
+public class AppParamsServices extends BaseServices<AppParamsDTO> {
+    Logger log = LoggerFactory.getLogger(AppParamsServices.class);
+    @Autowired
+    BaseBusinessInterface appParamsServices;
+
+    @PostConstruct
+    public void setupService() {
+        this.baseBusiness = appParamsServices;
+    }
+}
+
+
