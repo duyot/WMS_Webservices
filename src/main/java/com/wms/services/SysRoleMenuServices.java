450dd6f1bf475183c4f7b410db72feddcd44b3a5
truong_buixuan <truongcntt2@gmail.com>
2018-08-12 09:52:25 +0700

    update register form

--- src/main/java/com/wms/services/SysRoleMenuServices.java
+++ src/main/java/com/wms/services/SysRoleMenuServices.java
@@ -14,10 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -46,7 +43,7 @@ public class SysRoleMenuServices extends BaseServices<SysRoleMenuDTO> {
 
     @RequestMapping(value = "/getUserAction",produces = "application/json",method = RequestMethod.GET)
     @Cacheable("menus")
-    public List<ActionMenuDTO> getUserAction(@PathVariable("roleId") String roleId,@PathVariable("cusId") String cusId){
+    public List<ActionMenuDTO> getUserAction(@RequestParam("roleId") String roleId, @RequestParam("cusId") String cusId){
         List<SysMenuDTO> menus = getListUserAction(roleId,cusId);
         if(DataUtil.isListNullOrEmpty(menus)){
             return new ArrayList<>();
