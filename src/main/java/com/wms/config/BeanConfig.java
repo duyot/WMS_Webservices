09de57e49eefef4046a2c75cd6188c7f48af0a7b
truong_buixuan <truongcntt2@gmail.com>
2018-08-13 16:15:01 +0700

    disable jmx

--- src/main/java/com/wms/config/BeanConfig.java
+++ src/main/java/com/wms/config/BeanConfig.java
@@ -56,7 +56,6 @@ public class BeanConfig {
     @Bean(name = "transactionManager")
     public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
         HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
-
         return transactionManager;
     }
 
